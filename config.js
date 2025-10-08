1e2826c4dde27c661f5486adb27aec555eebcd48
DominicDev <s0l0estudi4r@gmail.com>
2025-10-08 18:16:20 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'HA-ASH';
+export let nameSongs = [
+	"LO APRENDÍ DE TI", /*1*/
+	"TE DEJO EN LIBERTAD", /*2*/
+	"EX DE VERDAD", /*3*/
+	"PERDÓN, PERDÓN", /*4*/
+	"SÉ QUE TE VAS", /*5*/
+	"NO PASA NADA", /*6*/
+	"QUÉ ME FALTÓ ?", /*7*/
+	"QUÉDATE LEJOS", /*8*/
+	"NO TE QUIERO NADA", /*9*/
+	"IMPERMEABLE", /*10*/
+	"ODIO AMARTE", /*11*/
+	"CORAZÓN IRROMPIBLE", /*12*/
+	"NO ME IMPORTA ", /*13*/
+	"ME GUSTAS TÚ", /*14*/
+	"PALETA", /*15*/
+	"100 AÑOS", /*16*/
+	"30 DE FEBRERO", /*17*/
+	"¿QUÉ HAGO YO?", /*18*/
+	"ESTÉS DONDE ESTÉS", /*19*/
+	"¿DE DÓNDE SACAS ESO?", /*20*/
+]
